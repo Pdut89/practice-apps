623b302a155d83cbac35ab8bb645fd8c67f1609d
Pieter <pieter_du_toit@hotmail.com>
2018-11-06 15:06:31 +0200

    add server tests

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -10,13 +10,12 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  console.log('here')
   const todo = new Todo({
     text: req.body.text
   });
 
   todo.save()
-  .then((doc) => {
+  .then(doc => {
     res.send(doc);
   }, (err) => {
     res.status(400).send(err);
@@ -26,3 +25,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {app}
