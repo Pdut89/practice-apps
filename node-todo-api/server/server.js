defe48c04193e0ac66362b14557bfb28a75fc6c4
Pieter <pdutoit@fullfacing.com>
2018-11-15 16:05:23 +0200

    add jwt

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,37 +1,39 @@
 require('./config/config')
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const {mongoose} = require('./db/mongoose');
-const {Todo} = require('./models/todo');
-const {User} = require('./models/user');
+const {mongoose} = require('./db/mongoose')
+const {Todo} = require('./models/todo')
+const {User} = require('./models/user')
 const {ObjectId} = require('mongodb')
 
-const app = express();
+const app = express()
 const port = process.env.PORT
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.post('/todos', (req, res) => {
   const todo = new Todo({
     text: req.body.text
-  });
+  })
 
   todo.save()
-  .then(doc => {
-    res.send(doc);
-  }, (err) => {
-    res.status(400).send(err);
-  });
-});
+    .then(todo => {
+      res.send(todo)
+    })
+    .catch(err => {
+      res.status(404).send(err)
+    })
+})
 
 app.get('/todos', (req, res) => {
   Todo.find()
     .then(todos => {
       res.send({todos})
-    }, err => {
-      res.status(400).send(err)
+    })
+    .catch(err => {
+      res.status(404).send(err)
     })
 })
 
@@ -59,7 +61,7 @@ app.delete('/todos/:id', (req, res) => {
       res.send({todo})
     })
     .catch(err => {
-      console.log(err)
+      res.status(404).send(err)
     })
 })
 
@@ -79,7 +81,6 @@ app.patch('/todos/:id', (req, res) => {
   Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
     .then(todo => {
       if (!todo) return res.status(404).send()
-
       res.send({todo})
     })
     .catch(err => {
@@ -87,8 +88,25 @@ app.patch('/todos/:id', (req, res) => {
     })
 })
 
+app.post('/users', (req, res) => {
+  const {email, password, tokens} = req.body
+  const user = new User({email, password})
+
+  user.save()
+    .then(() => {
+      return user.generateAuthToken()
+    })
+    .then(token => {
+      res.header('x-auth', token)
+      .send(user)
+    })
+    .catch(err => {
+      res.status(400).send(err)
+    })
+})
+
 app.listen(port, () => {
-  console.log(`Started on port ${port}`);
-});
+  console.log(`Started on port ${port}`)
+})
 
 module.exports = {app}
