151204cc340d53c2383a63d10568eaab58d591ea
Pieter <pdutoit@fullfacing.com>
2018-11-27 10:57:20 +0200

    add emit on createMessage

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -2,12 +2,6 @@ const socket = io()
 
 socket.on('connect', function() {
   console.log('Connected to server')
-
-  socket.emit('createMessage', {
-    to: 'pdut@gmail.com',
-    text: 'Hi pdut',
-    createdAt: new Date().toString()
-  })
 })
 
 socket.on('disconnect', function() {
--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -16,14 +16,13 @@ app.use(express.static(publicPath))
 io.on('connection', socket => {
   console.log('New user connected')
 
-  socket.emit('newMessage', {
-    from: 'mike@gmail.com',
-    text: 'Hey, what\'s up',
-    createdAt: '19 Nov 2018'
-  })
-
   socket.on('createMessage', message => {
     console.log('created message: ', message)
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   })
 
   socket.on('disconnect', () => {
