5b942d3b05222aabf2956473f9ce470fc59ce529
Pieter <pdutoit@fullfacing.com>
2018-11-27 09:14:27 +0200

    set up socket.io

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -1,13 +1,26 @@
 const express = require('express')
+const http = require('http')
 const path = require('path')
+const socketIO = require('socket.io')
+
+const port = process.env.PORT || 3000
 
 const app = express()
-const port = process.env.PORT || 3000
+const server = http.createServer(app)
+const io = socketIO(server)
 
 const publicPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicPath))
 
-app.listen(port, () => {
+io.on('connection', socket => {
+  console.log('New user connected')
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected')
+  })
+})
+
+server.listen(port, () => {
   console.log(`Started on port ${port}`)
 })
