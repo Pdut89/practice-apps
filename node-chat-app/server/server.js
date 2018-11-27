5750a39cef702bd8b7e6433a8a417b4d5b019d79
Pieter <pdutoit@fullfacing.com>
2018-11-27 11:28:51 +0200

    add generateMessage util

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -3,6 +3,8 @@ const http = require('http')
 const path = require('path')
 const socketIO = require('socket.io')
 
+const {generateMessage} = require('./utils/message')
+
 const port = process.env.PORT || 3000
 
 const app = express()
@@ -15,30 +17,14 @@ app.use(express.static(publicPath))
 
 io.on('connection', socket => {
   console.log('New user connected')
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chatroom!'))
 
-  socket.emit('newMessage', {
-    from: 'Admin',
-    text: 'Welcome to the chatroom!'
-  })
-
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'A new user has joined.'
-  })
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'A new user has joined the chatroom'))
 
   socket.on('createMessage', message => {
     console.log('created message: ', message)
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    })
 
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // })
+    io.emit('newMessage', generateMessage(message.from, message.text))
   })
 
   socket.on('disconnect', () => {
